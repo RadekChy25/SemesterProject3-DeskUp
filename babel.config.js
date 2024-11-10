c52b0154971039942208756330a5f8ec38c14ce2
Do <dojab23@student.sdu.dk>
2024-11-10 13:23:54 +0100

    login page git ignore

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+  ],
+  // Add sourceType: 'module' to ensure ES Modules are handled correctly
+  sourceType: 'module',
+};
---
+++ webpack.mix.js
@@ -0,0 +1,11 @@
+const mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'public/js')
+    .babelConfig({
+        presets: ['@babel/preset-env'], // Ensure Babel processes JS files
+        plugins: ['@babel/plugin-transform-modules-commonjs'] // Transpile ES Modules to CommonJS
+    })
+    .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss'),
+    ])
+    .sourceMaps();
