1cf6b9aff8105c93089cce3e352f5d897705cd14
RadekChy25 <chylak.radoslaw@gmail.com>
2024-11-10 13:29:07 +0100

    administration page

---
+++ resources/views/administration.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DeskUp</title>
+</head>
+<body>
+    <div class="Register">
+        <form action="">
+            <div class="name">
+                <label for="name">Name:</label>
+                <input type="text" id="name">
+            </div>
+            <div class="password">
+                <label for="password">Password:</label>
+                <input type="hidden" id="password">
+            </div>
+            <div>
+                <button type="submit">Register</button>
+            </div>
+        </form>
+
+        <div class="users">
+            <ol>
+                <li class="user">User</li>
+            </ol>
+        </div>
+
+        <div>
+            <button class="cleaningmode"></button>
+        </div>
+    </div>
+</body>
+</html>
