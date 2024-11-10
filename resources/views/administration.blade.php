ee195ed06c8aa12eae7b8613d48e80b93883a9be
RadekChy25 <chylak.radoslaw@gmail.com>
2024-11-10 15:43:07 +0100

    admin update

--- resources/views/administration.blade.php
+++ resources/views/administration.blade.php
@@ -4,31 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/administration.css') }}" />
     <title>DeskUp</title>
 </head>
 <body>
-    <div class="Register">
+    <h2>Register new users into the system</h2>
+    <div class="layout reg">
         <form action="">
             <div class="name">
-                <label for="name">Name:</label>
+                <label for="name">Name:</label><br>
                 <input type="text" id="name">
             </div>
             <div class="password">
-                <label for="password">Password:</label>
-                <input type="hidden" id="password">
+                <label for="password">Password:</label><br>
+                <input type="text" id="password">
             </div>
             <div>
                 <button type="submit">Register</button>
             </div>
         </form>
-
+    </div>
+    <div class="layout use">
         <div class="users">
             <ol>
                 <li class="user">User</li>
             </ol>
         </div>
-
-        <div>
+        
+        <div class="cleaning">
             <button class="cleaningmode"></button>
         </div>
     </div>
