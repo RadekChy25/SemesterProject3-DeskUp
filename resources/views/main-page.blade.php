96368017ed8722b4b1af5a1ba647cf680bedc947
Drifter <jacobsenpalouda@gmail.com>
2024-11-11 22:01:58 +0100

    Added buttons and linked stylesheet to main page

---
+++ resources/views/main-page.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>DeskUp Main Page</title>
+
+        <!-- Fonts -->
+        
+
+        <!-- Styles / Scripts -->
+        <link rel="stylesheet" href="../../assets/css/main-page.css">
+
+    </head>
+    <body>
+        <div>
+            <button class="arrow" id="up" name="up">Up</button>
+            <br>
+            <button class="arrow" id="down" name="down">Down</button>
+        </div>
+    </body>
+</html>
