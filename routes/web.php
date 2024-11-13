72e72a565fecc690172406f7394132ee22bef7fe
Do <dojab23@student.sdu.dk>
2024-11-13 13:54:10 +0100

    admin.index

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/user', [AuthController::class, 'getUser'])->middleware('auth');
 Route::get('/admin', function(){
     return view('admin');
-})->name('admin.blade.php');
+})->name('admin.index');
