f23faed91cc4826138c6901ea79e27ba5654153d
Do <dojab23@student.sdu.dk>
2024-11-13 13:09:12 +0100

    configuration and login page+favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
           <div class="flex items-center justify-between">
             <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
             <div class="text-sm">
-              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+              <a href="#" class="font-semibold text-grey-800 hover:text-indigo-500">Forgot password?</a>
             </div>
           </div>
           <div class="mt-2">
@@ -36,7 +36,9 @@
         </div>
   
         <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Sign in
+            </button>
         </div>
       </form>
     </div>
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,10 +2,11 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .babelConfig({
-        presets: ['@babel/preset-env'], // Ensure Babel processes JS files
-        plugins: ['@babel/plugin-transform-modules-commonjs'] // Transpile ES Modules to CommonJS
+        presets: ['@babel/preset-env'],
+        plugins: ['@babel/plugin-transform-modules-commonjs']
     })
     .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss'),
+        // Optional: You can also add other PostCSS plugins if needed
     ])
     .sourceMaps();
