f99d2dd1fb5dad60b504eacc1a792f8376c824e1
alsiepl <alan.sieradzki@o2.pl>
2024-11-17 12:37:22 +0100

    tailwind link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DeskUP</title>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-blue-500">
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
