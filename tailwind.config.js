f001b322c6d2270660a853a38985e860d2f403d6
alsiepl <alan.sieradzki@o2.pl>
2024-11-29 10:56:50 +0100

    plugis

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,45 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import plugin from 'tailwindcss/plugin';
+import { addIconSelectors } from '@iconify/tailwind';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-    './storage/framework/views/*.php',
-    './resources/**/*.blade.php',
-    './resources/**/*.js',
-    './resources/**/*.vue',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        'blue-1000': 'rgb(35, 4, 53)', // Custom blue color
-      },
-      borderRadius: {
-        // Adding custom border radius for circle
-        'rounded-circle': '50%', // This will create a circle if used on a square element
-      },
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  plugins: [
-    plugin(function({ addUtilities }) {
-        const newUtilities = {
-            '.text-justify': {
-                textAlign: 'justify',
-            },
-            '.text-center': {
-                textAlign: 'center',
-            },
-            '.text-left': {
-                textAlign: 'left',
-            },
-            '.text-right': {
-                textAlign: 'right',
-            },
-        };
+    plugins: [
+        plugin(function({ addUtilities }) {
+            const newUtilities = {
+                '.text-justify': {
+                    textAlign: 'justify',
+                },
+                '.text-center': {
+                    textAlign: 'center',
+                },
+                '.text-left': {
+                    textAlign: 'left',
+                },
+                '.text-right': {
+                    textAlign: 'right',
+                },
+            };
 
-        addUtilities(newUtilities, ['responsive', 'hover']);
-    }),
-    addIconSelectors({
-        // Ensure you have the correct configuration here
-        varName: 'iconify', // Example configuration
-    }),
-],
+            addUtilities(newUtilities, ['responsive', 'hover']);
+        }),
+        addIconSelectors({
+            // Ensure you have the correct configuration here
+            varName: 'iconify', // Example configuration
+        }),
+    ],
 };
