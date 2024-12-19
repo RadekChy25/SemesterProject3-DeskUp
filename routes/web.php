fd2fdb6149f30bc15e2ac35518778d98ec27635d
Do <dojab23@student.sdu.dk>
2024-12-19 10:48:33 +0100

    Revert "TimeDataControllerTest"
    This reverts commit 150b630eb7bd0b77ed8bde29f9de06129666dcea.

--- routes/web.php
+++ routes/web.php
@@ -51,4 +51,3 @@ Route::get('/activity', function () {
 })->name('activity');
 // routes/web.php
 Route::get('/getpresets', [PresetController::class, 'getPresets'])->name('getPresets');
-Route::get('/time-data', [TimeDataController::class, 'getTimeData'])->middleware('auth');
