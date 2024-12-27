ca5fa6b742c1cfb57b07346ca6b550a3420bac63
Erschh <tomecsko.ors@freemail.hu>
2024-12-27 19:47:57 +0100

    Implemented endtime scheduling

--- routes/console.php
+++ routes/console.php
@@ -10,64 +10,132 @@ const VERSION='v2/';
 const API_KEY="E9Y2LxT4g1hQZ7aD8nR3mWx5P0qK6pV7";
 
 
-Schedule::call(function()
+Schedule::call(function()//set cleaningmode
 {
     $desk_list=Http::get(URL.VERSION.API_KEY.'/desks'); //access the desks using the defined constant
     $desk_list=json_decode($desk_list);
+    
+    $set_to_after=0;
+
+    $modesets=Mode::where('name','cleaningmode')->first();
+    $height=$modesets->height*10;
 
     foreach ($desk_list as $desk) {
-        $out = new \Symfony\Component\Console\Output\ConsoleOutput();
-        $out->writeln($desk);
-
-        $modesets=Mode::where('name','cleaningmode')->first();
-        $height=$modesets->height*10;
-
+        $desk_info=Http::get(URL.VERSION.API_KEY.'/desks'.'/'.$desk);
+        $desk_info=json_decode($desk_info);
+        $set_to_after+=$desk_info->state->position_mm;
         $feedback=Http::put(URL.VERSION.API_KEY.'/desks'.'/'.$desk.'/state', //This is for the final version
         ['position_mm'=>$height]);
-        $out->writeln($feedback);
     }
+    $set_to_after/=count($desk_list);
+    $modesets->heightbefore=$set_to_after;
+    $modesets->save();
 })
     ->dailyAt((Mode::where('name', 'cleaningmode')->exists())?
     ((Mode::firstWhere('name', 'cleaningmode'))->start_time):'');
 
-Schedule:: call(
-    function()
-    {
-        if(Mode::where('name','cleaningmode')->exists()){
-            $out = new \Symfony\Component\Console\Output\ConsoleOutput();
-            $out->writeln((Mode::firstWhere('name', 'cleaningmode'))->start_time);
-        }
-    }  
-)->everyFifteenSeconds();
 
-Schedule::call(function()
+Schedule::call(function()//set fancymode
 {
     $desk_list=Http::get(URL.VERSION.API_KEY.'/desks'); //access the desks using the defined constant
     $desk_list=json_decode($desk_list);
+
+    $set_to_after=0;
 
     $modesets=Mode::where('name','fancymode')->first();
     $height=$modesets->height*10;
 
     foreach ($desk_list as $desk) {
+        $desk_info=Http::get(URL.VERSION.API_KEY.'/desks'.'/'.$desk);
+        $desk_info=json_decode($desk_info);
+        $set_to_after+=$desk_info->state->position_mm;
+
         $feedback=Http::put(URL.VERSION.API_KEY.'/desks'.'/'.$desk.'/state', //This is for the final version
         ['position_mm'=>$height]);
     }
+    $set_to_after/=$desk_list->count();
+    $modesets->heightbefore=$set_to_after;
+    $modesets->save();
 })
     ->dailyAt((Mode::where('name', 'fancymode')->exists())?
     (Mode::firstWhere('name','fancymode'))->start_time:'');
 
-Schedule::call(function()
+Schedule::call(function()//set discomode
 {
     $desk_list=Http::get(URL.VERSION.API_KEY.'/desks'); //access the desks using the defined constant
     $desk_list=json_decode($desk_list);
 
+    $set_to_after=0;
+
     $modesets=Mode::where('name','discomode')->first();
     $height=$modesets->height*10;
+
+    foreach ($desk_list as $desk) {
+        $desk_info=Http::get(URL.VERSION.API_KEY.'/desks'.'/'.$desk);
+        $desk_info=json_decode($desk_info);
+        $set_to_after+=$desk_info->state->position_mm;
+
+        $feedback=Http::put(URL.VERSION.API_KEY.'/desks'.'/'.$desk.'/state', //This is for the final version
+        ['position_mm'=>$height]);
+    }
+    $set_to_after/=$desk_list->count();
+    $modesets->heightbefore=$set_to_after;
+    $modesets->save();
+})
+    ->dailyAt((Mode::where('name', 'discomode')->exists())?
+    ((Mode::firstWhere('name', 'discomode'))->start_time):'');
+
+
+
+
+
+
+Schedule::call(function()//set after cleaningmode
+{
+    $desk_list=Http::get(URL.VERSION.API_KEY.'/desks'); //access the desks using the defined constant
+    $desk_list=json_decode($desk_list);
+
+    $modesets=Mode::where('name','cleaningmode')->first();
+    $height=$modesets->heightbefore;
 
     foreach ($desk_list as $desk) {
         $feedback=Http::put(URL.VERSION.API_KEY.'/desks'.'/'.$desk.'/state', //This is for the final version
         ['position_mm'=>$height]);
     }
 })
+    ->dailyAt((Mode::where('name', 'cleaningmode')->exists())?
+    ((Mode::firstWhere('name', 'cleaningmode'))->end_time):'');
+
+Schedule::call(function()//set after fancymode
+{
+    $desk_list=Http::get(URL.VERSION.API_KEY.'/desks'); //access the desks using the defined constant
+    $desk_list=json_decode($desk_list);
+
+    $modesets=Mode::where('name','fancymode')->first();
+    $height=$modesets->heightbefore;
+
+    foreach ($desk_list as $desk) {
+        $feedback=Http::put(URL.VERSION.API_KEY.'/desks'.'/'.$desk.'/state', //This is for the final version
+        ['position_mm'=>$height]);
+    }
+
+})
+    ->dailyAt((Mode::where('name', 'fancymode')->exists())?
+    ((Mode::firstWhere('name', 'fancymode'))->end_time):'');
+
+Schedule::call(function()//set after discomode
+{
+    $desk_list=Http::get(URL.VERSION.API_KEY.'/desks'); //access the desks using the defined constant
+    $desk_list=json_decode($desk_list);
+
+    $modesets=Mode::where('name','fancymode')->first();
+    $height=$modesets->heightbefore;
+
+    foreach ($desk_list as $desk) {
+        $feedback=Http::put(URL.VERSION.API_KEY.'/desks'.'/'.$desk.'/state', //This is for the final version
+        ['position_mm'=>$height]);
+    }
+
+})
     ->dailyAt((Mode::where('name', 'discomode')->exists())?
-    ((Mode::firstWhere('name', 'discomode'))->start_time):'');
+    ((Mode::firstWhere('name', 'discomode'))->end_time):'');
